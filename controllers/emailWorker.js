d0473426f101f7673ef76b210bfcb7df6507b840
Djain912 <darshan_arvind.jain@vpt.edu.in>
2024-09-07 13:08:14 +0530

    first commit

--- controllers/emailWorker.js
+++ controllers/emailWorker.js
@@ -19,7 +19,7 @@ parentPort.on('message', async (data) => {
 
     for (let subscriber of subscribers) {
       let mailOptions = {
-        from: '"CodeItUp" <https://codeitupblogs.netlify.app/>',
+        from: '"JainTrades" <https://jaintrades.netlify.app/>',
         to: subscriber.email,
         subject: 'New Blog Post Published!',
         text: `A new blog post has been published.\n\nTitle: ${title}\nSubtitle: ${subtitle}\nRead more at: ${url}`,
